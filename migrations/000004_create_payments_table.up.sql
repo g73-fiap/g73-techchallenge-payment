8b882124be3f261a3e833ba62a736a75c7a77df8
Igor Pestana <igor.pestana@greatcloudservices.com>
2024-05-12 15:48:37 -0300

    Removing unused files

--- migrations/000004_create_payments_table.up.sql
+++
@@ -1,7 +0,0 @@
--- Cria a tabela de pagamentos
-CREATE TABLE IF NOT EXISTS payments (
-    id UUID PRIMARY KEY,
-    amount DECIMAL(10, 2) NOT NULL,
-    description TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL
-);
